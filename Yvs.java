c048dfab29bce2c6f80a60033c1e85fbd5695442
yagantivenkates <62235122+yagantivenkates@users.noreply.github.com>
2020-03-17 17:49:44 +0530

    Update Yvs.java
    update the my name

--- Yvs.java
+++ Yvs.java
@@ -2,7 +2,7 @@ class Yvs
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("hello World!");
+		System.out.println("hello yaganti");
 		System.out.println("iam venkates");
 	}
 }
